6dc017b8c1af68e715ad9bc72b58943778cfbf8a
Buchi-dev-dzn <buchi.dev.dzn@gmail.com>
2026-07-02 01:09:01 +0900

    Initial Homebrew formula

---
+++ Casks/port.rb
@@ -0,0 +1,11 @@
+cask "port" do
+  version "0.1.0"
+  sha256 "..."
+
+  url "https://github.com/Buchi-dev-dzn/port-releases/releases/download/v0.1.0/Port.app.zip"
+  name "Port"
+  desc "Modern terminal workspace"
+  homepage "https://github.com/Buchi-dev-dzn/Port"
+
+  app "Port.app"
+end
