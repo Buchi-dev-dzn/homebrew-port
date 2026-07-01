3fc48ea558a8975df38e64fcd9cfc51369882841
Buchi-dev-dzn <buchi.dev.dzn@gmail.com>
2026-07-02 01:14:15 +0900

    マイナーチェンジ

--- Casks/port.rb
+++ Casks/port.rb
@@ -1,8 +1,9 @@
 cask "port" do
   version "0.1.0"
-  sha256 "..."
+  sha256 "9a4200e71118f7d06ce780925285c20ded167dbe92da33556d15e7b14a3ba82b"
 
   url "https://github.com/Buchi-dev-dzn/port-releases/releases/download/v0.1.0/Port.app.zip"
+
   name "Port"
   desc "Modern terminal workspace"
   homepage "https://github.com/Buchi-dev-dzn/Port"
