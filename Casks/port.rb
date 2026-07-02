4bc4003c0c328c0ce6912c690596b598827f3386
Buchi-dev-dzn <buchi.dev.dzn@gmail.com>
2026-07-03 00:50:43 +0900

    v0.1.3

--- Casks/port.rb
+++ Casks/port.rb
@@ -1,6 +1,6 @@
 cask "port" do
   version "0.1.3"
-  sha256 "e832983ea244a08191a1b9f3f0b7d3c9c263189f9bb6954eed21547db8f6a613"
+  sha256 "43737a158c5a6972ba97d37ed0ea43a150f8a404e0ae69dc6413f34ccee9ef14"
 
   url "https://github.com/Buchi-dev-dzn/port-releases/releases/download/v0.1.3/Port.app.zip"
 
