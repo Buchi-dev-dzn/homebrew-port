003057483a42401078c9d7bf248db970681fa9d7
Buchi-dev-dzn <buchi.dev.dzn@gmail.com>
2026-07-07 12:17:10 +0900

    vO.1.6

--- Casks/port.rb
+++ Casks/port.rb
@@ -1,8 +1,8 @@
 cask "port" do
-  version "0.1.5"
-  sha256 "25b64b3e6276a137816680b70ae86b30bb623c2261b58f5b493b72844bde37b5"
+  version "0.1.6"
+  sha256 "ab6f400c6f13fc583fc44078d3df9121a4086cc2cc5cfc0c6e54eb11eec80982"
 
-  url "https://github.com/Buchi-dev-dzn/port-releases/releases/download/v0.1.5/Port.app.zip"
+  url "https://github.com/Buchi-dev-dzn/port-releases/releases/download/v0.1.6/Port.app.zip"
 
   name "Port"
   desc "Modern terminal workspace"
